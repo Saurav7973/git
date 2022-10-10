0e9afbb99d8ab109467a74c59f2769cdc386a153
saurav7973 <saurav@techies.it>
2022-10-10 15:49:12 +0530

    branch commit new

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         println("second commit")
         println("third commit")
-        println("new example ")
+        println("new  ")
 
     }
 }
